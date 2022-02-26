19c7cc08f5d5bb2f50df6541796585e989e4bc54
lyszkowski-dominik <57920532+lyszkowski-dominik@users.noreply.github.com>
2022-02-26 21:48:40 +0100

    Complete Overhaul
    Complete overhaul of program, split into specific functions

--- translator.py
+++ translator.py
@@ -1,39 +1,16 @@
 from bs4 import BeautifulSoup
-from translatorFunc import getPage
+from translatorFunc import getPage, displayMenu, filterWords, presentData
 
 
 translationWay = {'en': 'french-english', 'fr': 'english-french'}
 languages = {'en': 'English', 'fr': 'French'}
-words = []
-sentences1 = []
-sentences2 = []
-language = input('Type "en" if you want to translate from French into English, or "fr" if you want to translate from English into French:')
-word = input("Type the word you want to translate:")
-print('You chose "{}" as a language to translate "{}" to'.format(language, word))
 
-translation = translationWay[language]
-# get translation page
-r = getPage(translation, word)
-soup = BeautifulSoup(r.content, 'html.parser')
-# filter translated words
-for para in soup.find_all(class_='translation'):
-    words.append(para.get_text().strip())
-words.remove("Translation")
-print("{} Translations:".format(languages[language]))
-# print only 5 translated words
-print(*words[0:5], sep="\n")
-# filter translated sentence native
-for para in soup.find_all('div', 'src ltr'):
-    sentences1.append(para.get_text().strip())
-# filter translated sentence target
-for para in soup.find_all('div', 'trg ltr'):
-    sentences2.append(para.get_text().strip())
 
-print("\n")
-print("{} Examples:".format(languages[language]))
-# print 5 translated sentences native / target
-for i in range(5):
-    print(sentences1[i])
-    print(sentences2[i])
-    print("\n")
+choices = displayMenu()
 
+# Get page with translation
+r = getPage(choices[0], choices[1])
+site = BeautifulSoup(r.content, 'html.parser')
+
+output = filterWords(site)
+presentData(output, choices)
--- translatorFunc.py
+++ translatorFunc.py
@@ -5,21 +5,54 @@ headers = {'User-Agent': 'Mozilla/5.0'}
 
 def getPage(translation, word):
     address = 'https://context.reverso.net/translation/{}/{}'.format(translation, word)
-    print(address)
     r = requests.get(address, headers=headers)
     if r.status_code == 200:
-        print(str(r.status_code) + " OK")
         return r
     else:
         print("Error while getting data")
 
 
 def displayMenu():
+    languages = ['', 'arabic', 'german', 'english', 'spanish', 'french', 'hebrew', 'japanese', 'dutch', 'polish',
+                 'portuguese', 'romanian', 'russian', 'turkish']
     print("Hello, you're welcome to the translator. Translator supports: ")
-    print(
-        "1. Arabic\n2. German\n3. English\n4. Spanish\n5. French\n6. Hebrew\n7. Japanese\n8. Dutch\n9. Polish\n10. Portuguese\n11. Romanian\n12. Russian\n13. Turkish")
-    a = input("Type the number of your language:")
-    b = input("Type the number of language you want to translate to:")
+    for i in range(1, 14):
+        print(str(i) + ". " + languages[i].capitalize())
+    a = languages[int(input("Type the number of your language:"))]
+    b = languages[int(input("Type the number of language you want to translate to:"))]
     word = input("Type the word you want to translate:")
-    fromTo = [a, b, word]
+    direction = a + '-' + b
+    fromTo = [direction, word, b]
     return fromTo
+
+
+def filterWords(site):
+    words = []
+    sentencesFrom = []
+    sentencesTo = []
+    # filter translated words
+    for para in site.find_all(class_='translation'):
+        words.append(para.get_text().strip())
+    words.remove("Translation")
+    # filter translated sentence native
+    for para in site.find_all('div', 'src ltr'):
+        sentencesFrom.append(para.get_text().strip())
+    # filter translated sentence target
+    for para in site.find_all('div', 'trg ltr'):
+        sentencesTo.append(para.get_text().strip())
+    extractedData = [words, sentencesFrom, sentencesTo]
+    return extractedData
+
+
+def presentData(data, choices):
+    print("{} Translations:".format(choices[2].capitalize()))
+    # print only 5 translated words
+    for i in range(5):
+        print(data[0][i])
+    print("\n")
+    print("{} Examples:".format(choices[2].capitalize()))
+    # print 5 translated sentences native / target
+    for i in range(5):
+        print(data[1][i])
+        print(data[2][i])
+        print("\n")
